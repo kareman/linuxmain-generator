22a593dad9c729eb9d431b3af9c3804356a0b7d9
Kare Morstol <kare@nottoobadsoftware.com>
2016-10-12 20:45:34 +0200

    makeAllTests.

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,10 +11,12 @@ extension String {
 	}
 }
 
-typealias TestFunctions = [(class: String, funcs: [String])]
+typealias TestFunctions = (classname: String, funcs: [String])
+let indentation = "\t"
 
-func getTestFunctions(_ input: ReadableStream) -> TestFunctions {
-	var result = TestFunctions()
+
+func getTestFunctions(_ input: ReadableStream) -> [TestFunctions] {
+	var result = [TestFunctions]()
 	for line in input.lines() {
 		if line.contains("XCTestCase"),
 			let classname = line.subString(between: "class", and: ":")?.trimmingCharacters(in: .whitespaces) {
@@ -32,8 +34,21 @@ func getTestFunctions(_ input: ReadableStream) -> TestFunctions {
 	return result
 }
 
+func makeAllTests(_ testfunctions: TestFunctions) -> String {
+	var result = "\nextension " + testfunctions.classname + " {\n"
+	result += indentation + "static var allTests = [\n"
+	result += testfunctions.funcs.map { testfunc in
+		indentation + indentation + "(\"\(testfunc)\", \(testfunc)),\n"
+		}.joined()
+	result += indentation + indentation + "]\n"
+	result += "}\n"
+	return result
+}
+
 guard let swiftfile = try main.arguments.first.map ({try open($0)}) else {
 	exit(errormessage: "Missing argument for swift file")
 }
 
-print(getTestFunctions(swiftfile))
+print(makeAllTests(getTestFunctions(swiftfile).last!))
+
+
