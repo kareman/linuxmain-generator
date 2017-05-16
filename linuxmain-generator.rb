2fc068f666e0ba1e16642d03baf084229a6cf0ba
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2017-05-16 22:02:57 +0100

    fixed brew formula issue

--- linuxmain-generator.rb
+++ linuxmain-generator.rb
@@ -2,6 +2,7 @@ class LinuxmainGenerator < Formula
   desc "Shell command to keep SPM tests in sync on OSX and Linux."
   homepage "https://github.com/valeriomazzeo/linuxmain-generator"
   url "https://github.com/valeriomazzeo/homebrew-linuxmain-generator/archive/0.2.0.tar.gz"
+  version "0.2.0"
   sha256 "50e0ec57794191b830fd2c69aff8bbfd65989d0c837809b68035bcafd9e8da09"
 
   def install
